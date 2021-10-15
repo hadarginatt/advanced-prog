d3bea5c60694a72ca813f55f2d23f7e014826ed6
dar <mendelowitz10@gmail.com>
2021-10-15 10:51:37 +0300

    part 1.1

---
+++ ‫‪anomaly_detection_util.cpp
@@ -0,0 +1,62 @@
+#include "‫‪anomaly_detection_util.h"
+#include <cmath>
+using namespace std;
+
+float mean(float* x, int size) {
+    float num = 0;
+    for (int i = 0; i < size; i++) {
+        num += x[i];
+    }
+    return (float) (num / (float) size);
+}
+
+float deviation(float var) {
+    return sqrt(var);
+}
+
+float var(float* x, int size) {
+    float m = mean(x, size), num = 0;
+    for (int i = 0; i < size; i++) {
+        num += x[i] * x[i];
+    }
+    return (float) (num / (float) size) - m * m;
+}
+
+float cov(float* x, float* y, int size) {
+    float num = 0;
+    for (int i = 0; i < size; i++) {
+        num += x[i] * y[i];
+    }
+    return num / (float) size - mean(x, size) * mean(y, size);
+}
+
+float pearson(float* x, float* y, int size) {
+    float covar = cov(x, y, size);
+    float devX = deviation(var(x, size));
+    float devY = deviation(var(y, size));
+
+    return (float) covar / devX * devY;
+}
+
+Line linear_reg(Point** points, int size) {
+    float x[size], y[size];
+
+    for (int i = 0; i < size; i++) {
+        x[i] = points[i]->x;
+        y[i] = points[i]->y;
+    }
+
+    float co = cov(x, y, size);
+    float a = co / var(x, size);
+    float b = mean(y, size) - a * mean(x, size);
+
+    return Line(a, b);
+}
+
+float dev(Point p, Point** points, int size) {
+    return dev(p, linear_reg(points, size));
+}
+
+float dev(Point p, Line l) {
+    return abs(l.f(p.x) - p.y);
+}
