de263ab25f5bacd593ca0e003ff8d079d1cfd1dc
dar <mendelowitz10@gmail.com>
2021-10-20 17:40:30 +0300

    part 1.1 ids

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ bool wrong(float val, float expected) {
     return val < expected - 0.001 || val > expected + 0.001;
 }
 
-// this is a simple test to put you on the right track
+// this is a simple test to put you on the right track /
 int main() {
     const int N = 10;
     float x[] = {1,2,3,4,5,6,7,8,9,10};
