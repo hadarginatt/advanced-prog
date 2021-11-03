fa8ec3e88ba1dad7d6764a3a8218bb3d5ccb5331
dar <mendelowitz10@gmail.com>
2021-11-03 09:58:43 +0200

    part 1.2

--- AnomalyDetector.h
+++ AnomalyDetector.h
@@ -1,24 +1,6 @@
 #ifndef ADVANCED_PROG_ANOMALYDETECTOR_H
 #define ADVANCED_PROG_ANOMALYDETECTOR_H
 
-#include <string>
-#include <vector>
-using namespace std;
 
-class TimeSeries {};
-
-class AnomalyReport {
-public:
-    const string description;
-    const long timeStep;
-    AnomalyReport(string description, long timeStep) : description(description), timeStep(timeStep) {}
-};
-
-class TimeSeriesAnomalyDetector {
-public:
-    virtual void learnNormal(const TimeSeries& ts) = 0;
-    virtual vector<AnomalyReport> detect(const TimeSeries& ts) = 0;
-    virtual ~TimeSeriesAnomalyDetector() {};
-};
 
 #endif //ADVANCED_PROG_ANOMALYDETECTOR_H
