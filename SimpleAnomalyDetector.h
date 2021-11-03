b0f71900e4edfd83388a4d784ea9d8a2904303fa
dar <mendelowitz10@gmail.com>
2021-11-03 15:52:33 +0200

    part 1.2

--- SimpleAnomalyDetector.h
+++ SimpleAnomalyDetector.h
@@ -1,5 +1,25 @@
 #ifndef ADVANCED_PROG_SIMPLEANOMALYDETECTOR_H
 #define ADVANCED_PROG_SIMPLEANOMALYDETECTOR_H
+#include <string>
+#include "anomaly_detection_util.h"
 #include "AnomalyDetector.h"
+using namespace std;
+
+struct correlatedFeatures {
+    string feature1, feature2;
+    float corrlation;
+    Line lin_reg;
+    float threshold;
+};
+
+class SimpleAnomalyDetector: public TimeSeriesAnomalyDetector {
+public:
+    SimpleAnomalyDetector();
+    virtual ~SimpleAnomalyDetector();
+
+    virtual void learnNormal(const TimeSeries& ts);
+    virtual vector<AnomalyReport> detect(const TimeSeries& ts);
+    vector<correlatedFeatures> getNormalModel();
+};
 
 #endif //ADVANCED_PROG_SIMPLEANOMALYDETECTOR_H
