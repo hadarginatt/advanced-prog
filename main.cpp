58aed13823e8926135c6f40284185fb021dc7d46
dar <mendelowitz10@gmail.com>
2021-11-03 15:37:26 +0200

    part 1.2

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 bool wrong(float val, float expected) {
     return val < expected - 0.001 || val > expected + 0.001;
 }
+//
 
 // this is a simple test to put you on the right track /
 int main() {
