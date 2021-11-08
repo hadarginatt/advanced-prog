5cf59a25bd45ba306fbcfa1e7fbc5283d44fbd96
dar <mendelowitz10@gmail.com>
2021-11-08 11:51:39 +0200

    part 1.2

--- timeseries.h
+++ timeseries.h
@@ -14,6 +14,7 @@ class TimeSeries {
     vector<string> features;
     friend void learnNormal(const TimeSeries& ts);
 
+    //
 public:
     explicit TimeSeries(const char* fileName) {
         ifstream file(fileName);
