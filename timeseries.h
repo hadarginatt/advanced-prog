6e82e8ca893426c1f654a09db75d01dc926e0d14
dar <mendelowitz10@gmail.com>
2021-11-08 12:09:03 +0200

    part 1.2 time

--- timeseries.h
+++ timeseries.h
@@ -14,50 +14,16 @@ class TimeSeries {
     friend void learnNormal(const TimeSeries& ts);
 
 public:
-    explicit TimeSeries(const char* fileName) {
-        ifstream file(fileName);
-        string feature, value;
-
-        // Adding all of the features listed in the file into the features vector.
-        while (getline(file, feature, ',')) {
-            features.push_back(feature);
-
-            // Adding a vector of floats to each feature in _map.
-            vector<float> floats;
-            _map[feature] = floats;
-        }
-
-        int size = features.size();
-
-        for (string line; getline(file, line); ) {
-            stringstream st(line);
-            int i = 0;
-
-            // Adding the values in a line to their matching feature.
-            while (getline(st, value, ',') && i < size) {
-                string s = features[i];
-                _map[s].push_back(stof(value));
-                i++;
-            }
-        }
-
-        file.close();
-    };
+    explicit TimeSeries(const char* fileName);
 
     // Returns a list of the features.
-    vector<string> getFeatures() {
-        return features;
-    }
+    vector<string> getFeatures();
 
     // Returns a list of values associated with a feature.
-    const vector<float>& getValues(const string& feature) const {
-        return _map.at(feature);
-    }
+    const vector<float>& getValues(const string& feature) const;
 
     // Returns the i'th value of a feature (the object in line i, row j).
-    float getValueByTimeStep(string& feature, int i) {
-        return _map.at(feature).at(i);
-    }
+    float getValueByTimeStep(string& feature, int i);
 
     ~TimeSeries() = default;;
 };
